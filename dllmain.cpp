08b84e50a43323d474778967e9f76b4f90421c33
nigger <nigger@floyd.fent>
2025-07-21 22:10:28 +0400

    visual studio trash g++ better

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,17 +3,9 @@
 #include <stdio.h>
 
 void WINAPI INIT() {
-    HANDLE hRead, hWrite;
-
-    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
 
     AllocConsole();
     freopen("CONOUT$", "w", stdout);
-
-
-    if (!CreatePipe(&hRead, &hWrite, &sa, 0)) return;
-
-    if (!SetStdHandle(STD_OUTPUT_HANDLE, hWrite) || !SetStdHandle(STD_ERROR_HANDLE, hWrite)) return;
 
     printf(R"EOF(
             _......._
@@ -38,20 +30,10 @@ void WINAPI INIT() {
           `-..___..-`
           
     )EOF");
-
-    printf("\n");
-
-    DWORD bytesRead;
-    char buffer[256];
-    while (ReadFile(hRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL) && bytesRead > 0) {
-        buffer[bytesRead] = '\0';
-        printf("%s", buffer);
-    }
-
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved){
-    if (ul_reason_for_call) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         HANDLE hMainThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)INIT, NULL, 0, NULL);
     }
     return TRUE;
